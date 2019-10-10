15986aa69a8337ae2db14c3ba76149fda3237c00
Savas <savas@moneyboxapp.com>
2019-10-10 16:43:03 +0100

    Added hash generation

--- HMAC.Win/Form1.Designer.cs
+++ HMAC.Win/Form1.Designer.cs
@@ -28,20 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.requestTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hashButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // requestTextBox
+            // 
+            this.requestTextBox.Location = new System.Drawing.Point(155, 37);
+            this.requestTextBox.Multiline = true;
+            this.requestTextBox.Name = "requestTextBox";
+            this.requestTextBox.Size = new System.Drawing.Size(952, 1184);
+            this.requestTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Request";
+            // 
+            // hashButton
+            // 
+            this.hashButton.Location = new System.Drawing.Point(987, 1240);
+            this.hashButton.Name = "hashButton";
+            this.hashButton.Size = new System.Drawing.Size(120, 61);
+            this.hashButton.TabIndex = 2;
+            this.hashButton.Text = "Hash!";
+            this.hashButton.UseVisualStyleBackColor = true;
+            this.hashButton.Click += new System.EventHandler(this.hashButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(155, 1317);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(952, 29);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 1320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hash";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1719, 1412);
+            this.ClientSize = new System.Drawing.Size(1158, 1403);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.hashButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.requestTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox requestTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button hashButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- HMAC.Win/Form1.cs
+++ HMAC.Win/Form1.cs
@@ -1,20 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Globalization;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HMAC.Win
 {
     public partial class Form1 : Form
     {
+        private const string WebHookSecret = "put the secret here";
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void hashButton_Click(object sender, System.EventArgs e)
+        {
+            var dataToBeHashed = requestTextBox.Text;
+            //var dataToBeHashed = requestTextBox.Text.Replace("\n", "").Replace("\t", "");
+            resultTextBox.Text = HashHMACHex(WebHookSecret, dataToBeHashed);
+        }
+
+        private string HashHMACHex(string keyHex, string message)
+        {
+            var hash = HashHMAC(HexDecode(keyHex), StringEncode(message));
+            return HashEncode(hash);
+        }
+
+        private byte[] HashHMAC(byte[] key, byte[] message)
+        {
+            var hmac = new HMACSHA256(key);
+            return hmac.ComputeHash(message);
+        }
+
+        private byte[] StringEncode(string text)
+        {
+            return Encoding.UTF8.GetBytes(text);
+        }
+
+        private string HashEncode(byte[] hash)
+        {
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+
+        private byte[] HexDecode(string hex)
+        {
+            var bytes = new byte[hex.Length / 2];
+            for (int i = 0; i < bytes.Length; i++)
+                bytes[i] = byte.Parse(hex.Substring(i * 2, 2), NumberStyles.HexNumber);
+
+            return bytes;
         }
     }
 }
